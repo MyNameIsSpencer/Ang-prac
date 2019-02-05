f1b86cd3a8236b118ba95868fd9fb47cce612028
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-04 20:40:30 -0500

    ang folder

--- prac/likeMain.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var liker_1 = require("./liker");
-var component = new liker_1.Liker(10, true);
-component.onClick();
-console.log("likesCount: " + component.likesCount + ", isSelected: " + component.isSelected);
--- prac/likeMain.ts
+++
@@ -1,5 +0,0 @@
-import { Liker } from './liker';
-
-let component = new Liker(10, true);
-component.onClick();
-console.log(`likesCount: ${component.likesCount}, isSelected: ${component.isSelected}`);
