c6a029263936254abf3b30de175e74802df93052
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-05 18:27:25 -0500

    set up form initialization

--- VALIDATIONS/src/app/app.component.ts
+++ VALIDATIONS/src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements ngOnInit {
   contactForm: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit() {
+    this.initForm();
+  }
+//  all address inputs are grouped
+  initForm() {
+    this.contactForm = this.formBuilder.group({
+      username: [
+        '',
+        [Validators.required, Validators.minLength(5), Validators.maxlength(10)]
+      ],
+      fullname: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      cpassword: ['', Validators.required],
+      address: this.formBuilder.group({
+        address1: ['', Validators.required],
+        address2: ['', Validators.required],
+        zipcode: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        country: ['', Validators.required]
+      })
+    });
+  }
 }
