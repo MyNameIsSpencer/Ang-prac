aa26161e9f19e86b63412026589e05866db5e42c
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-07 17:52:50 -0500

    password match validation

--- VALIDATIONS/src/app/app.component.ts
+++ VALIDATIONS/src/app/app.component.ts
@@ -23,19 +23,42 @@ export class AppComponent implements OnInit {
       ],
       fullname: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@^$#&!%*?&])[A-Za-zd$#^@$&!%*?&].{8,}') ]],
       cpassword: ['', Validators.required],
       address: this.formBuilder.group({
         address1: ['', Validators.required],
         address2: ['', Validators.required],
-        zipcode: ['', Validators.required],
+        zipcode: ['', [Validators.required, Validators.pattern('(?=.*[0-9])') ]],
         city: ['', Validators.required],
         state: ['', Validators.required],
         country: ['', Validators.required]
       })
+    }, {
+      validator: this.ConfirmPassword.bind(this)
     });
   }
   onSubmit() {
     console.log(this.contactForm);
   }
+
+  ConfrimPassword(pwdFormGroup: FormGroup) {
+    // const pwd = pwdFormGroup.controls['pass']
+    const pwd = pwdFormGroup.controls.password.value;
+    const cpwd = pwdFormGroup.controls.cpassword.value;
+
+    if (cpwd.length <= 0) {
+      return null;
+    }
+
+    if (pwd !== cpwd) {
+      return {
+        doesNotMatch: true;
+      };
+    }
+
+    return null;
+  }
 }
+
+
+// errors: {doesNotMatch: true}
