6758adc73bb8d0cc4d139a7d66467688cbd523e1
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-08 17:57:02 -0500

    set up cors

--- pdfdir/PDFFRONTEND/src/app/app.module.ts
+++ pdfdir/PDFFRONTEND/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 const routes: Routes = [
   {
