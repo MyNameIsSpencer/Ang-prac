e37ea47e88e2f840c4331cb35bdbfc548eaf544d
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-14 22:55:16 -0500

    express ()

--- login/backendLOGIN/index.js
+++ login/backendLOGIN/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express;
+const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/loginApp', {
