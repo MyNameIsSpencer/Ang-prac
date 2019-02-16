fda58f2002516d811eef465fa155c1874aee800a
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-15 22:45:40 -0500

    add signup/ files

--- login/backendLOGIN/index.js
+++ login/backendLOGIN/index.js
@@ -4,6 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
+const signup = require('./signup/regRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/loginApp', {
   useNewUrlParser: true
@@ -12,21 +20,3 @@ mongoose.connect('mongodb://localhost/loginApp', {
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
-
-
-
-
-//
-// //Import the mongoose module
-// var mongoose = require('mongoose');
-//
-// //Set up default mongoose connection
-// var mongoDB = 'mongodb://127.0.0.1/my_database';
-// mongoose.connect(mongoDB);
-// // Get Mongoose to use the global promise library
-// mongoose.Promise = global.Promise;
-// //Get the default connection
-// var db = mongoose.connection;
-//
-// //Bind connection to error event (to get notification of connection errors)
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
