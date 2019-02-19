4b7289de5816014462d844b3530acaf9be96d666
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-18 23:54:05 -0500

    encrypt password, check if email exists

--- login/backendLOGIN/signup/regCtrl.js
+++ login/backendLOGIN/signup/regCtrl.js
@@ -1,1 +1,40 @@
-exports.CreateUser = (req, res) => { console.log(req.body); };
+const Joi = require('joi');
+
+const User = require('../models/user');
+
+exports.CreateUser = async (req, res) => {
+  const schema = Joi.object().keys({
+    username: Joi.string().min(5).max(50).required(),
+    email: Joi.string().email().require(),
+    password: Joi.string().min(5).required()
+  });
+
+  const { error, value } = Joi.validate(req.body, schema);
+  if (error && error.details) {
+    return res.status(400).json({ msg: error.details });
+  }
+
+  const email = await User.findOne({ email: req.body.email.toLowerCase() });
+  if (email) {
+    reutrn res.status(409).json({message: 'User email already exists' });
+  }
+
+
+  const username = await User.findOne({ username: req.body.username.toLowerCase() });
+  if (username) {
+    reutrn res.status(409).json({ message: 'Username already exists' });
+  }
+
+  const body = {
+    username: value.username,
+    email: value.email.toLowerCase(),
+    password: value.password
+  };
+  User.create(body)
+    .then(user => {
+      res.status(201).json({ message: 'User created successfully', user });
+    })
+    .catch(err => {
+      res.status(500).json({ message: err });
+    })
+};
