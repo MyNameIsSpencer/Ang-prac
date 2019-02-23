f6c387d140fed72aec87c1512d45d719a9979e32
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-23 14:23:22 -0500

    randomize resetToken

--- password-reset/backendPASSWORDRESET/controllers/forgot-password.js
+++ password-reset/backendPASSWORDRESET/controllers/forgot-password.js
@@ -1,3 +1,25 @@
+const User = require('../models/userModel');
+
 module.exports = {
-  Forgotpassword(req, res) {}
+  async Forgotpassword(req, res) {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        res.status(404).json({ error: 'User not found' });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
+
+function ResetToken(num) {
+  const string = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let str = '';
+  let i = 0;
+  while (i < num) {
+    str += string.charAt(Math.floor(Math.random() * string.length));
+    i++;
+  }
+  return str;
 }
